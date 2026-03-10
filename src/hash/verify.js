dc1f93cb31b77ddf1322895bd6c3d4a2a9f370df
Alejandro Duarte <alejandrod.92@gmail.com>
2026-03-10 18:28:20 -0500

    Add dynamic and verify functions

--- src/hash/verify.js
+++ src/hash/verify.js
@@ -1,8 +1,29 @@
+import crypto from 'crypto'
+import fs, { open } from 'fs/promises'
+import stream from 'stream/promises';
+
 const verify = async () => {
-  // Write your code here
   // Read checksums.json
   // Calculate SHA256 hash using Streams API
   // Print result: filename — OK/FAIL
+  const checksums = await fs.readFile('src/hash/checksums.json', 'utf8');
+  const checksumsObj = JSON.parse(checksums);
+
+  for (const [filename, expectedHash] of Object.entries(checksumsObj)) {
+    const fileHandle = await open(`${process.cwd()}/src/hash/${filename}`);
+    const fileStream = fileHandle.createReadStream();
+    const hash = crypto.createHash('sha256');
+
+    await stream.pipeline(fileStream, hash);
+    const calculatedHash = hash.digest('hex');
+
+    if (calculatedHash === expectedHash) {
+      console.log(`${filename} — OK`);
+    } else {
+      console.log(`${filename} — FAIL`);
+    }
+    await fileHandle.close();
+  }
 };
 
 await verify();
